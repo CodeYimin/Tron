dc52fc8e1e559903e6dccdc2077a7b5625d5bb68
rickyqin005 <69991001+rickyqin005@users.noreply.github.com>
2022-05-13 14:36:41 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import misc.Consts;
 
 public class Main {
     public static void main(String[] args) {
-        Game game = new Game("Tron", Consts.WINDOW_WIDTH, Consts.WINDOW_HEIGHT);
+        new Game("Tron", Consts.WINDOW_WIDTH, Consts.WINDOW_HEIGHT);
     }
 }
